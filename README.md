86aa257e25687f8a99cc49146f4df400d7fc9bfa
root <root@smx>
2024-05-11 00:56:31 +0800

    markdown init && git

---
+++ README.md
@@ -0,0 +1,8 @@
+# 关于这个笔记系统
+
+本笔记系统使用github action中push后执行脚本的特性，通过在push后执行node.js脚本实现将markdown转换成html并整合到index.html中。
+
+一共有两个分支，一个main分支是存放笔记html，一个是markdown分支，用来存放编写的markdown文本的。
+
+
+本笔记系统的核心是精简，所有笔记通过ctrl + f在浏览器页面中可以快速定位。
---
+++ transfer.js
@@ -0,0 +1,42 @@
+var fs = require('fs')
+
+var m = {};
+var htstr = "";
+
+process.argv.forEach(function (val, index, array) {
+    // val : ./2024.5/xx.html
+    // arr: [".","2024.5","xx.html"]
+    var arr = val.split('/')
+    if (arr.length == 3) {
+        if (!m[arr[1]]) m[arr[1]] = [];
+        m[arr[1]].push({ txt: arr[2], href: val });
+    }
+});
+/*
+ m:{
+     '2024.1':[{txt:xxx.html,href:'./2024.1/xx.html'},{...}],
+     '2024.2':...
+ }
+*/
+
+// sort keys 
+var keys = Object.keys(m).sort((a, b) => { return parseInt(a.replace('.', '')) - parseInt(b.replace('.', '')) })
+var yearSet = {};
+
+var sectionstr = "<section>";
+
+for (const date_ of Object.keys(m)) {
+    // 遍历每个数组元素的属性
+    m[date_].forEach((element) => {
+        // 访问每个元素的属性
+        sectionstr += 
+        `
+        <p><a href='${element.href}'>${date_}_${element.txt}</a></p>
+        `;
+
+    });
+}
+sectionstr += "</section>"
+
+htstr="<div id=\"content\" class=\"container\">\n"+sectionstr+"\n</div>"
+fs.writeFileSync('./index.html', fs.readFileSync('./index.html').toString().replace(/<div id="content"([\s\S])*\/div>/gm, htstr));
